0931b189ec193a63df3a945e2e70bf9fb181b0b2
jitendra <jithendrapitchuka@gmail.com>
2024-08-18 22:31:24 +0530

    saving user

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,15 +1,28 @@
 package com.scm.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.scm.entities.User;
+import com.scm.forms.UserForm;
+import com.scm.services.UserService;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
 
 @Controller
 public class PageController {
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping("/home")
     public String home(Model model) {
@@ -28,12 +41,34 @@ public class PageController {
 
     @RequestMapping("/signup")
     public String signupPage(Model model) {
+
+        UserForm userForm=new UserForm();
+        model.addAttribute( "userForm",userForm);
+
         return "register";
     }
 
     @RequestMapping("/login")
     public String loginPage(Model model) {
         return "login";
+    }
+    
+    @PostMapping("/do-register")
+    public String processRegister(@ModelAttribute UserForm userForm) {
+        System.out.println(userForm);
+        User user=User.builder()
+        .name(userForm.getName())
+        .email(userForm.getEmail())
+        .password(userForm.getPassword())
+        .about(userForm.getAbout())
+        .phoneNumber(userForm.getPhoneNumber())
+        .build();
+        
+        User savedUser = userService.saveUser(user);
+        
+        System.out.println(savedUser);
+
+        return "redirect:/signup";
     }
     
 
